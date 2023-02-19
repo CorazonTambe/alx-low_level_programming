3bba7b7f5b1076f1a32b6b8e1957aa85e3e0de50
CorazonTambe <Corazon.enow@gmail.com>
2023-02-19 09:56:56 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,8 +4,10 @@
  * Description: prints the alphabet in lowercase\n
  * Return: Always 0 (sucess)
  */
+
 int main(void)
 {
+
 int ch;
 
 for (ch = 'a'; ch <= 'z'; ch++)
@@ -13,4 +15,5 @@ for (ch = 'a'; ch <= 'z'; ch++)
 putchar('\n');
 }
 return (0);
+
 }
