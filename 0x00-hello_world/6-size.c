7ed69d74112ef47b6e9af86e1a357cf387a49e4b
CorazonTambe <Corazon.enow@gmail.com>
2023-02-19 12:04:12 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,14 @@
 /**
  * main - prints the size of various types based on
  * the computer it is compiled and ran on
- * Return: Always (0)
+ * Return: Always 0 success
  */
 int main(void)
 {
-printf("Size of a char: 1 byte\n", sizeof(char));
-printf("size of an int: 4 byte(s)\n", sizeof(int));
-printf("Size of a long int: 4 bytes(s)\n", sizeof(long int));
-printf("Size of a long long int: 8 bytes(s)\n", sizeof(long long int));
-printf("Size of a float: 4 bytes(s)\n", sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %zubytes(s)\n", sizeof(float));
 return (0);
 }
