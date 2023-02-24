a51025b7591eb0ad58ba2df97346fb9196bff033
CorazonTambe <Corazon.enow@gmail.com>
2023-02-24 07:12:17 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,23 +7,23 @@
  */
 
 void print_to_98(int n)
-}
+{
 	int i;
 
-		if (n <= 98)
-		{
-		for (i = n; i < 98; i++)
-		{
-			printf("%d, ", i);
-		}
-		}
-		else
-		{
-		for (i = n; i > 98; i--)
-		{
-			rintf("%d, ", i);
-		}
-		}
+	if (n <= 98)
+	{
+	for (i = n; i < 98; i++)
+	{
+	printf("%d, ", i);
+	}
+	}
+	else
+	{
+	for (i = n; i > 98; i--)
+	{
+	printf("%d, ", i);
+	}
+	}
 
-			printf("98\n");
+	printf("98\n");
 }
