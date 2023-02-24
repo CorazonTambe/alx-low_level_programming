dd813d62ca77a998def8d7e912378d1fd438e7f5
CorazonTambe <Corazon.enow@gmail.com>
2023-02-24 14:09:26 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _issupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _issupper(int c)
+{
+if (c >= "A" && c <= "Z")
+return (1);
+else
+return (0);
+}
