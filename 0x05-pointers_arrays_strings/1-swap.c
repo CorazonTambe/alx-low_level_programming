c6cbe3630aeb52d60da9d17faf7bc8ab35d47021
CorazonTambe <Corazon.enow@gmail.com>
2023-02-28 15:06:18 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length
- * Return: The length of @str.
+ * swap_int - Swaps the values of two integers.
+ * @a: The first integer to be swapped.
+ * @b: The second integer to be swapped.
+ *
+ * Return: nothing
  */
-size_t _strlen(const char *str)
-{
-size_t length = 0;
 
-while (*str++)
-length++;
-return (length);
+void swap_int(int *a, int *b)
+/* the function that swaps the values of two integers. */
+{
+int temp = *a;
+*a = *b;
+*b = temp;
 }
