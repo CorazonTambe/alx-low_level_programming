bf62547c21276b71dd3b3c0e5e5fc9367a3b0e17
CorazonTambe <Corazon.enow@gmail.com>
2023-03-09 01:28:10 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * primeFinder - searches for if a number is prime
+ *
+ * @a: the number to check
+ * @b: the numbers we'll go through
+ *
+ * Return: Whether or not the number is a prime number
+ */
+
+int primeFinder(int a, int b)
+{
+	if (a <= 1 || a % b == 0)
+		return (0);
+	else if (a == b)
+		return (1);
+	else if (a > b)
+		primeFinder(a, b + 1);
+
+	return (1);
+}
